925795406980b2ca1cdacf52d70a6c7411773a1b
xankush <ankushdohare12@gmail.com>
2025-08-04 10:00:28 +0530

    add the shuffling algo

--- src/main/resources/static/js/quiz.js
+++ src/main/resources/static/js/quiz.js
@@ -1,18 +1,29 @@
 const radios = document.getElementsByName("option");
 console.log(radios[1].value);
 const optionspanid = ["optionone","optiontwo","optionthree","optionfour"]
+
 var selectedoption =null
 var question = null ;
 let correctanswer ;
 const questiontext = document.getElementById("quiztext");
-const urlbasepath=" https://7315974104d5.ngrok-free.app";
-//const urlbasepath="http://localhost:8080";
-let id = 1;
+//const urlbasepath=" https://7315974104d5.ngrok-free.app";
+const urlbasepath="http://localhost:8080";
+const quizid = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+let id =0;
+function startquiz(){
+	shuffleArray(quizid);
+	getquestiondata();
+	
+}
+
+startquiz();
+
 
 async function getquestiondata(){
 	
-	
-	fetch(`${urlbasepath}/getquestion/${id}`)
+	if(id<15){
+		
+	fetch(`${urlbasepath}/getquestion/${quizid[id]}`)
 	  .then(response => response.json()) // Convert response to JSON
 	  .then(data => {question = data;
 		
@@ -30,9 +41,10 @@ async function getquestiondata(){
 	  .catch(error => console.error('Fetch error:', error));
 	  id++;
 	  console.log(id);
+	}
   }
 
-getquestiondata();
+
 
 console.log(question)
   
@@ -63,6 +75,18 @@ async function getselectedansweer(){
 		console.log("correct")
 	}
 	console.log(correctanswer)
+}
+
+function shuffleArray(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    // Pick a random index from 0 to i
+    const j = Math.floor(Math.random() * (i + 1));
+
+    // Swap arr[i] with arr[j]
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  console.log(arr);
+  return arr;
 }
 
 
